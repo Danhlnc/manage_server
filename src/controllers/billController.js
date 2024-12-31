b8dcf294a871586b64baefac4f5e6a5dc5f4f4ae
Danhlnc <danhlnc8433@gmail.com>
2025-01-01 01:37:45 +0700

    Update billController.js

--- src/controllers/billController.js
+++ src/controllers/billController.js
@@ -5,6 +5,10 @@ const getAll = async (req, res, next) => {
     try {
 
         const bills = await billService.getAll(req)
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+        res.setHeader('Access-Control-Allow-Credentials', true); 
         res.status(StatusCodes.OK).json(bills)
     } catch (error) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
@@ -16,6 +20,10 @@ const getDrinkAll = async (req, res, next) => {
     try {
 
         const bills = await billService.getDrinkAll(req)
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+        res.setHeader('Access-Control-Allow-Credentials', true); 
         res.status(StatusCodes.OK).json(bills)
     } catch (error) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
@@ -27,6 +35,10 @@ const getTaboccoAll = async (req, res, next) => {
     try {
 
         const bills = await billService.getTaboccoAll(req)
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+        res.setHeader('Access-Control-Allow-Credentials', true); 
         res.status(StatusCodes.OK).json(bills)
     } catch (error) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
@@ -39,7 +51,10 @@ const addOne = async (req, res) => {
 
         const bills = await billService.addOne(req, res)
         if (bills) {
-
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+            res.setHeader('Access-Control-Allow-Credentials', true); 
             res.status(StatusCodes.OK).json(bills)
         }
     } catch (error) {
@@ -53,7 +68,10 @@ const updateOne = async (req, res) => {
 
         const bills = await billService.updateOne(req, res)
         if (bills) {
-
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+            res.setHeader('Access-Control-Allow-Credentials', true); 
             res.status(StatusCodes.OK).json(bills)
         }
     } catch (error) {
