7b2cfd12f71b2b03b01cc108abc2a59204447f16
Danhlnc <danhlnc8433@gmail.com>
2025-01-01 01:36:00 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,24 @@ app.use(cors({
   origin: '*',
   allowedHeaders: 'X-Requested-With, Content-Type, auth-token',
 }));
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 import express from 'express'
 import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 import { mapOrder } from '~/utils/sorts.js'
