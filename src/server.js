f7488aa7446ce18de220cdb3cc7fe8c4f23f49ee
Danhlnc <danhlnc8433@gmail.com>
2025-01-01 01:48:10 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,28 +1,7 @@
 
 const cors = require("cors");
 
-app.use(cors({
-  origin: '*',
-  allowedHeaders: 'X-Requested-With, Content-Type, auth-token',
-}));
-app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 import express from 'express'
 import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 import { mapOrder } from '~/utils/sorts.js'
@@ -34,7 +13,28 @@ const START_SERVER = () => {
   const app = express()
   //router
   app.use(express.json())
+  app.use(cors({
+    origin: '*',
+    allowedHeaders: 'X-Requested-With, Content-Type, auth-token',
+  }));
+  app.use(function (req, res, next) {
   
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  
+    // Pass to next layer of middleware
+    next();
+  });
   app.use('/v1', APIs_V1)
   app.get('/', async (req, res) => {
     console.log(env.AUTHOR);
